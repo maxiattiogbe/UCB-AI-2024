902b555056978fea026d5307907ee8c98a0d331c
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-06-22 16:16:53 -0700

    convert score into a POST request

--- src/app/api/score/route.ts
+++ src/app/api/score/route.ts
@@ -6,16 +6,22 @@ const hume = new HumeClient({
   apiKey: process.env.HUME_API_KEY,
 });
 
-// Get the score training session,
+/* Get the score training session
+   - Provide a url to the video recording
+
+   todo: Probably have it get it by id and then check mongodb to see if it exists
+   if it does then return the already processed, if it doesn't then process it
+   so probably change this back to a GET request
+*/
 export async function POST(req: Request) {
-  const shet = await req.json();
-  console.log(shet);
+  const { url } = await req.json();
+  console.log(url);
 
   const job = await hume.expressionMeasurement.batch.startInferenceJob({
     models: {
       face: {},
     },
-    urls: ["https://hume-tutorials.s3.amazonaws.com/faces.zip"],
+    urls: [url],
   });
 
   console.log("Running...");
