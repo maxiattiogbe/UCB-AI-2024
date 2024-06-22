803504bfcddc867880e3641329d4d8b8e86b695d
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-06-22 13:42:15 -0700

    test husky

--- src/views/default.tsx
+++ src/views/default.tsx
@@ -1,20 +1,33 @@
-'use client';
+"use client";
 
 import { useBoolean } from "@/hooks/use-boolean";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
 import Image from "next/image";
 
 export default function DefaultPage() {
-    const isDialogOpened = useBoolean(true)
-    return <main>
-    <Dialog open={isDialogOpened.value} fullWidth maxWidth="sm">
-      <DialogTitle>Test dialog</DialogTitle>
-      <DialogContent>
-      </DialogContent>
-      <DialogActions>
-        <Button>Cancel</Button>
-        <Button color="primary" variant="soft" onClick={isDialogOpened.onFalse}>Example button</Button>
-      </DialogActions>
-    </Dialog>
-  </main>
+  const isDialogOpened = useBoolean(true);
+  return (
+    <main>
+      <Dialog open={isDialogOpened.value} fullWidth maxWidth="sm">
+        <DialogTitle>Test dialog</DialogTitle>
+        <DialogContent></DialogContent>
+        <DialogActions>
+          <Button>Cancel</Button>
+          <Button
+            color="primary"
+            variant="soft"
+            onClick={isDialogOpened.onFalse}
+          >
+            Example button
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </main>
+  );
 }
