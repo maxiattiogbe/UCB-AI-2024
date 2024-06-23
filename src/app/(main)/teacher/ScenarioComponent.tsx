22f32ddcfde5d39a7e746dc210d05548f1bd41ce
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-06-23 11:44:20 -0700

    fix default value bug

--- src/app/(main)/teacher/ScenarioComponent.tsx
+++ src/app/(main)/teacher/ScenarioComponent.tsx
@@ -153,6 +153,7 @@ function CustomCard({ caseInfo, handleDelete, handleEdit }) {
                   <input
                     type="text"
                     id={field.id}
+                    defaultValue={caseInfo[field.id]}
                     className="flex-1 py-2 px-3 border border-gray-300 rounded-md"
                   />
                 </div>
